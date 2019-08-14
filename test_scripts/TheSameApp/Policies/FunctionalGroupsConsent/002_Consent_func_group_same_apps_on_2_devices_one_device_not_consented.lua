6e86bfe26b492e9fccf7cbb854276437bbc801f8
Oleksandr Deriabin <aderiabin@luxoft.com>
2019-08-14 12:55:52 +0300

    fixup! Add User consent related scripts for the same app from multiple devices feature

--- test_scripts/TheSameApp/Policies/FunctionalGroupsConsent/002_Consent_func_group_same_apps_on_2_devices_one_device_not_consented.lua
+++ test_scripts/TheSameApp/Policies/FunctionalGroupsConsent/002_Consent_func_group_same_apps_on_2_devices_one_device_not_consented.lua
@@ -116,7 +116,7 @@ runner.Title("Preconditions")
 runner.Step("Clean environment", common.preconditions)
 runner.Step("Prepare preloaded PT", common.modifyPreloadedPt, {modificationOfPreloadedPT})
 runner.Step("Start SDL and HMI", common.start)
-runner.Step("Connect two mobile devices to SDL", connectMobDevices, {devices})
+runner.Step("Connect two mobile devices to SDL", connectMobDevices)
 runner.Step("Register App1 from device 1", common.registerAppEx, {1, appParams[1], 1})
 runner.Step("Register App1 from device 2", common.registerAppEx, {2, appParams[1], 2})
 

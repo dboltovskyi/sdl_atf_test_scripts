1f6a498c0b678d8a9427d98d815fff237434fcba
Dmitriy Boltovskiy <dboltovskyi@luxoft.com>
2019-08-15 01:09:19 -0400

    Rework VehicleData scripts

--- test_scripts/API/VehicleData/OnVehicleData/001_Success_flow.lua
+++ test_scripts/API/VehicleData/OnVehicleData/001_Success_flow.lua
@@ -16,7 +16,6 @@
 ---------------------------------------------------------------------------------------------------
 
 --[[ Required Shared libraries ]]
-local runner = require('user_modules/script_runner')
 local common = require('test_scripts/API/VehicleData/commonVehicleData')
 
 --[[ Local Variables ]]
@@ -60,15 +59,15 @@ local rpc2 = {
 
 local vehicleDataResults = {
   engineOilLife = {
-    dataType = "VEHICLEDATA_ENGINEOILLIFE", 
+    dataType = "VEHICLEDATA_ENGINEOILLIFE",
     resultCode = "SUCCESS"
   },
   fuelRange = {
-    dataType = "VEHICLEDATA_FUELRANGE", 
+    dataType = "VEHICLEDATA_FUELRANGE",
     resultCode = "SUCCESS"
   },
   tirePressure = {
-    dataType = "VEHICLEDATA_TIREPRESSURE", 
+    dataType = "VEHICLEDATA_TIREPRESSURE",
     resultCode = "SUCCESS"
   },
   electronicParkBrakeStatus = {
@@ -76,42 +75,41 @@ local vehicleDataResults = {
     resultCode = "SUCCESS"
   },
   turnSignal = {
-    dataType = "VEHICLEDATA_TURNSIGNAL", 
+    dataType = "VEHICLEDATA_TURNSIGNAL",
     resultCode = "SUCCESS"
   }
 }
 
 --[[ Local Functions ]]
-local function processRPCSubscribeSuccess(self)
-  local mobileSession = common.getMobileSession(self, 1)
-  local cid = mobileSession:SendRPC(rpc1.name, rpc1.params)
-  EXPECT_HMICALL("VehicleInfo." .. rpc1.name, rpc1.params)
+local function processRPCSubscribeSuccess()
+  local cid = common.getMobileSession():SendRPC(rpc1.name, rpc1.params)
+  common.getHMIConnection():ExpectRequest("VehicleInfo." .. rpc1.name, rpc1.params)
   :Do(function(_, data)
-      self.hmiConnection:SendResponse(data.id, data.method, "SUCCESS", vehicleDataResults)
+      common.getHMIConnection():SendResponse(data.id, data.method, "SUCCESS", vehicleDataResults)
     end)
 
   local responseParams = vehicleDataResults
   responseParams.success = true
   responseParams.resultCode = "SUCCESS"
-  mobileSession:ExpectResponse(cid, responseParams)
+  common.getMobileSession():ExpectResponse(cid, responseParams)
 end
 
-local function checkNotificationSuccess(self)
-  local mobileSession = common.getMobileSession(self, 1)
-  self.hmiConnection:SendNotification("VehicleInfo." .. rpc2.name, rpc2.params)
-  mobileSession:ExpectNotification("OnVehicleData", rpc2.params)
+local function checkNotificationSuccess()
+  common.getHMIConnection():SendNotification("VehicleInfo." .. rpc2.name, rpc2.params)
+  common.getMobileSession():ExpectNotification("OnVehicleData", rpc2.params)
 end
 
 --[[ Scenario ]]
-runner.Title("Preconditions")
-runner.Step("Clean environment", common.preconditions)
-runner.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
-runner.Step("RAI with PTU", common.registerAppWithPTU)
-runner.Step("Activate App", common.activateApp)
+common.Title("Preconditions")
+common.Step("Clean environment", common.preconditions)
+common.Step("Start SDL, HMI, connect Mobile, start Session", common.start)
+common.Step("RAI", common.registerApp)
+common.Step("PTU", common.policyTableUpdate, { common.ptUpdate })
+common.Step("Activate App", common.activateApp)
 
-runner.Title("Test")
-runner.Step("RPC " .. rpc1.name, processRPCSubscribeSuccess)
-runner.Step("RPC " .. rpc2.name, checkNotificationSuccess)
+common.Title("Test")
+common.Step("RPC " .. rpc1.name, processRPCSubscribeSuccess)
+common.Step("RPC " .. rpc2.name, checkNotificationSuccess)
 
-runner.Title("Postconditions")
-runner.Step("Stop SDL", common.postconditions)
+common.Title("Postconditions")
+common.Step("Stop SDL", common.postconditions)
